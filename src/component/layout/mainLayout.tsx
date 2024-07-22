68b7be3ed9595fa11f5b9a885d55fd3b88372943
yoouyeon <jyeon.yoon59@gmail.com>
2024-07-23 00:29:05 +0900

    Design: 임시 기본 페이지 레이아웃 적용

---
+++ src/component/layout/mainLayout.tsx
@@ -0,0 +1,29 @@
+import { Box, VStack } from '@chakra-ui/react';
+import Header from './header';
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => (
+  <VStack
+    spacing={'0'}
+    h={'fit-content'}
+    w={'100vw'}
+    minH={'100vh'}
+    maxW={'1280px'} // Note : 왜 1280px로 설정했는지?
+  >
+    <Header />
+    <Box as={'main'} flexGrow={'1'} w={'100%'}>
+      {children}
+    </Box>
+    <Box
+      as={'footer'}
+      h={'32'}
+      w={'100%'}
+      mb={'20'}
+      mt={'12'}
+      bgColor={'orange'}
+    >
+      footer
+    </Box>
+  </VStack>
+);
+
+export default MainLayout;
