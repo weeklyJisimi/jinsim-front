c0d37b86687bc28f9bbb45f9e7894309f77708e3
yoouyeon <jyeon.yoon59@gmail.com>
2024-07-23 01:12:49 +0900

    Design: 카카오 로그인 버튼

---
+++ src/app/login/kakaoLoginButton.tsx
@@ -0,0 +1,23 @@
+import { Button, ButtonProps } from '@chakra-ui/react';
+import KakaoSymbol from '@/component/icon/kakaoSymbol';
+
+const KakaologinButton = (props: ButtonProps) => (
+  <Button
+    {...props}
+    h={'45px'}
+    fontSize={'15px'}
+    borderRadius={'12px'}
+    textColor={'#000000D9'}
+    bgColor={'#FEE500'}
+    _hover={{
+      bgColor: '#FFEB3B',
+    }}
+    px={'14px'}
+    py={'11px'}
+    leftIcon={<KakaoSymbol boxSize={'18px'} color={'#000000'} />}
+  >
+    카카오 로그인
+  </Button>
+);
+
+export default KakaologinButton;
---
+++ src/component/icon/kakaoSymbol.tsx
@@ -0,0 +1,21 @@
+// https://developers.kakao.com/docs/latest/ko/kakaologin/design-guide
+// https://www.figma.com/community/file/1232637617420363657
+import { Icon, IconProps } from '@chakra-ui/react';
+
+const KakaoSymbol = (props: IconProps) => (
+  <Icon viewBox="0 0 200 200" {...props}>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#clip0_103_60)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M12.0001 0.800003C5.37225 0.800003 0 4.95061 0 10.0697C0 13.2534 2.07788 16.06 5.24205 17.7293L3.91072 22.5927C3.7931 23.0224 4.28457 23.3649 4.66197 23.1159L10.4978 19.2643C10.9903 19.3118 11.4908 19.3395 12.0001 19.3395C18.6274 19.3395 24 15.1891 24 10.0697C24 4.95061 18.6274 0.800003 12.0001 0.800003Z"
+          fill="black"
+          fill-opacity="0.85"
+        />
+      </g>
+    </svg>
+  </Icon>
+);
+
+export default KakaoSymbol;
