6b0bb9d958cc441b75aa1b31689aa123f8451d89
yoouyeon <jyeon.yoon59@gmail.com>
2024-07-23 01:30:04 +0900

    Design: 임시 로그인 페이지 레이아웃 구성

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+import { Heading, VStack } from '@chakra-ui/react';
+import KakaologinButton from './kakaoLoginButton';
+
+const Login = () => {
+  return (
+    // NOTE : center에 위치하는 컨텐츠의 최대 너비는 모바일 화면을 기준으로 600px으로 설정함 (논의 필요...)
+    <VStack w={'100%'} maxW={'600px'} spacing={'4'} alignItems={'stretch'}>
+      <Heading as={'span'} fontSize={'2xl'}>
+        로그인 페이지
+      </Heading>
+      <KakaologinButton />
+    </VStack>
+  );
+};
+
+export default Login;
