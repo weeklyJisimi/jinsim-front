3009bf7fa352f58419b96d6828d7b889b19525a9
yoouyeon <jyeon.yoon59@gmail.com>
2024-07-27 00:29:14 +0900

    Feat: 좌우 이동 버튼 추가

--- src/app/letter/@create/page.tsx
+++ src/app/letter/@create/page.tsx
@@ -10,9 +10,11 @@ import {
   Input,
   Button,
   EditableTextarea,
+  IconButton,
 } from '@chakra-ui/react';
 import { useSourceStore } from '../letter-source-store';
 import { useLetterStore } from '../letter-store';
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 
 const Page = () => {
   const { source } = useSourceStore();
@@ -33,16 +35,20 @@ const Page = () => {
         </HStack>
         <Button>{`저장 ${DUMMYSAVECOUNT}/6`}</Button>
       </HStack>
-      <Editable
-        defaultValue={
-          '사전 질문의 답변으로 사용자 문체를 파악하고 아키네이터가 질문지 답변을 바탕으로 내용을 구성하여 편지 1을 보여줍니다. 그리고 사용자가 후속으로 직접 수정할 수 있으며 완성된 편지는 사용자 데이터로 남아 기존의 사용자 데이터 + 아카이빙 데이터 형식으로 누적됩니다.'
-        }
-        w={'100%'}
-        h={'15em'}
-      >
-        <EditablePreview />
-        <EditableTextarea h={'100%'} />
-      </Editable>
+      <HStack w={'100%'} justifyContent={'space-between'}>
+        <IconButton aria-label="이전 편지" icon={<ChevronLeftIcon />} />
+        <Editable
+          defaultValue={
+            '사전 질문의 답변으로 사용자 문체를 파악하고 아키네이터가 질문지 답변을 바탕으로 내용을 구성하여 편지 1을 보여줍니다. 그리고 사용자가 후속으로 직접 수정할 수 있으며 완성된 편지는 사용자 데이터로 남아 기존의 사용자 데이터 + 아카이빙 데이터 형식으로 누적됩니다.'
+          }
+          w={'100%'}
+          h={'15em'}
+        >
+          <EditablePreview />
+          <EditableTextarea h={'100%'} />
+        </Editable>
+        <IconButton aria-label="다음 편지" icon={<ChevronRightIcon />} />
+      </HStack>
       <HStack>
         <Text>From.</Text>
         <Input placeholder={'편지 쓰는 대상을 적어주세요'} />
