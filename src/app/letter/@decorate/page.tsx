0ba3421f604571a8c66f0035dd5308ec770a4ac6
yoouyeon <jyeon.yoon59@gmail.com>
2024-07-27 02:53:52 +0900

    Feat: 하단 버튼 추가

--- src/app/letter/@decorate/page.tsx
+++ src/app/letter/@decorate/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { Text, Heading, VStack, Box } from '@chakra-ui/react';
+import { Text, Heading, VStack, Box, Button, HStack } from '@chakra-ui/react';
 import { useLetterStore } from '../letter-store';
 import { useState } from 'react';
 import { useToPng } from '@hugocxl/react-to-image';
+import { useLetterFlowStore } from '../letter-flow-store';
 
 const Page = () => {
+  const { setState } = useLetterFlowStore();
   const { letter, currentLetterIndex } = useLetterStore();
   const [imageSrc, setImageSrc] = useState<string | undefined>();
   const [{ status, isLoading }, convertToImage, ref] = useToPng<HTMLDivElement>(
@@ -35,7 +37,7 @@ const Page = () => {
           }
           ref={ref}
         >
-          <Heading>{letter[currentLetterIndex].title}</Heading>
+          <Heading mx={'auto'}>{letter[currentLetterIndex].title}</Heading>
           <Text
             fontWeight={'bold'}
           >{`To. ${letter[currentLetterIndex].to}`}</Text>
@@ -48,6 +50,14 @@ const Page = () => {
             fontWeight={'bold'}
           >{`From. ${letter[currentLetterIndex].from}`}</Text>
         </VStack>
+        <HStack>
+          <Button onClick={() => setState('create')}>
+            <Text>편지 수정</Text>
+          </Button>
+          <Button>
+            <Text>편지 완성하기</Text>
+          </Button>
+        </HStack>
       </VStack>
     </>
   );
