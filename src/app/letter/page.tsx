86db83cb8f24623c0a3888350daeeaed64e4daa6
yoouyeon <jyeon.yoon59@gmail.com>
2024-07-27 06:57:45 +0900

    Design: 편지 생성 질문 수집 페이지 디자인

--- src/app/letter/page.tsx
+++ src/app/letter/page.tsx
@@ -3,19 +3,27 @@
 import Jinsimi from '@/component/common/jinsimi';
 import { Heading, VStack, Text, Button } from '@chakra-ui/react';
 import { useLetterFlowStore } from './letter-flow-store';
+import FairyJinsimi from '@/component/common/FairyJinsimi';
 
 const Page = () => {
   const { setState } = useLetterFlowStore();
   return (
     <VStack>
-      <Heading>진심이와 함께 편지를 써볼까요?</Heading>
+      <Heading
+        bg={'#FF6000'}
+        p={'26px'}
+        rounded={'30px'}
+        fontWeight={'black'}
+        textColor={'white'}
+      >
+        진심이와 함께 편지를 써볼까요?
+      </Heading>
       <Button
         h={'fit-content'}
         variant={'unstyled'}
         onClick={() => setState('initial')}
       >
-        <Jinsimi h={'15rem'} />
-        <Text>진심이를 클릭하여 편지 쓰러 가기!</Text>
+        <FairyJinsimi />
       </Button>
     </VStack>
   );
