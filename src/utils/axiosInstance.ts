4eb0f779708c3ba3b59b602d5861b793a2c8788a
Hyeon Ji Kim <94117828+hyeon81@users.noreply.github.com>
2024-07-27 07:27:04 +0900

    [Login] 문체 생성 API 추가 (#23)

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -3,10 +3,3 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
-
-export const axiosInstanceWithAuth = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-  },
-});
