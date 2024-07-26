1ef3e74fcbe2a0f1fa4eed96fe85d3c68b4bb4cd
yoouyeon <jyeon.yoon59@gmail.com>
2024-07-27 02:48:03 +0900

    Feat: 기본 편지지 컴포넌트 추가

--- src/app/letter/@decorate/page.tsx
+++ src/app/letter/@decorate/page.tsx
@@ -1,5 +1,56 @@
+'use client';
+
+import { Text, Heading, VStack, Box } from '@chakra-ui/react';
+import { useLetterStore } from '../letter-store';
+import { useState } from 'react';
+import { useToPng } from '@hugocxl/react-to-image';
+
 const Page = () => {
-  return <div>Letter Decorate Page</div>;
+  const { letter, currentLetterIndex } = useLetterStore();
+  const [imageSrc, setImageSrc] = useState<string | undefined>();
+  const [{ status, isLoading }, convertToImage, ref] = useToPng<HTMLDivElement>(
+    {
+      onStart: () => {
+        console.log('onStart');
+      },
+      onSuccess: (data) => {
+        setImageSrc(data);
+      },
+      onError: (error) => {
+        console.error(error);
+      },
+    }
+  );
+  return (
+    <>
+      <VStack>
+        {/* 편지 이미지 컴포넌트 */}
+        <VStack
+          p={2}
+          spacing={4}
+          alignItems={'flex-start'}
+          w="1024px"
+          backgroundImage={
+            'https://cdn.pixabay.com/photo/2023/02/01/21/40/pink-7761356_1280.png'
+          }
+          ref={ref}
+        >
+          <Heading>{letter[currentLetterIndex].title}</Heading>
+          <Text
+            fontWeight={'bold'}
+          >{`To. ${letter[currentLetterIndex].to}`}</Text>
+          <Box>
+            {letter[currentLetterIndex].body.split('\n').map((line, index) => (
+              <Text key={index}>{line}</Text>
+            ))}
+          </Box>
+          <Text
+            fontWeight={'bold'}
+          >{`From. ${letter[currentLetterIndex].from}`}</Text>
+        </VStack>
+      </VStack>
+    </>
+  );
 };
 
 export default Page;
