4e41d81cab89841021014605dbc2d36a7292dc23
Hyeon Ji Kim <94117828+hyeon81@users.noreply.github.com>
2024-07-27 12:51:18 +0900

    편지함 api 연결

--- src/app/letter/view/[id]/page.tsx
+++ src/app/letter/view/[id]/page.tsx
@@ -2,35 +2,43 @@
 
 import { useParams, useRouter } from 'next/navigation';
 import Header from '@/component/layout/header';
-import { VStack, Heading, Image, Button, HStack } from '@chakra-ui/react';
+import { VStack, Heading, Image, Button, HStack, Box } from '@chakra-ui/react';
 import ShareLetterButton from '@/component/common/shareLetter';
 import Link from 'next/link';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { Message } from '@/app/letter/view/page';
 
 const LetterDetailPage = () => {
   const params = useParams();
 
-  const removeLetter = () => {
-    //@todo letterId로 편지 삭제
-    alert(`${params?.id}가 삭제되었습니다.`);
-  };
+  const { data } = useQuery<Message>({
+    queryKey: ['letterDetail', params?.id],
+    queryFn: async () =>
+      await axios(`${process.env.NEXT_PUBLIC_API_URL}/letter/${params?.id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      }).then((res) => res.data),
+  });
 
   return (
-    <VStack gap={'1rem'} w={'100%'} h={'100vh'}>
+    <VStack gap={'1rem'} w={'100%'} h={'100vh'} justify={'center'}>
       <Header />
       <VStack spacing={4}>
-        <Heading size={'lg'}>제목</Heading>
-        <Image
-          objectFit={'contain'}
-          w={'100%'}
-          h={'100%'}
-          alt={'편지 이미지'}
-          src={'https://picsum.photos/200/300'}
-        />
+        <Heading size={'lg'}>{data?.title}</Heading>
+        <Box w={'30%'}>{data?.content}</Box>
+        {/*<Image*/}
+        {/*  objectFit={'contain'}*/}
+        {/*  w={'100%'}*/}
+        {/*  h={'100%'}*/}
+        {/*  alt={'편지 이미지'}*/}
+        {/*  src={'https://picsum.photos/200/300'}*/}
+        {/*/>*/}
+        <HStack>To. {data?.recipient}</HStack>
+        <HStack>{data?.createdAt}</HStack>
         <HStack>
-          <Button onClick={removeLetter}>삭제하기</Button>
           <ShareLetterButton />
-        </HStack>
-        <HStack>
           <Link href={'/letter/view'}>
             <Button>목록으로</Button>
           </Link>
--- src/app/letter/view/page.tsx
+++ src/app/letter/view/page.tsx
@@ -16,48 +16,49 @@ import Link from 'next/link';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
+export interface Message {
+  content: string;
+  createdAt: string; // ISO 8601 date string
+  id: number;
+  isDraft: boolean;
+  isPublic: boolean;
+  recipient: string;
+  title: string;
+}
+
 const View = () => {
-  const { isPending, error, data } = useQuery({
+  const { data: data2, isPending } = useQuery({
     queryKey: ['viewData'],
     queryFn: async () =>
-      await axios(`${process.env.NEXT_PUBLIC_API_URL}/letter/view`),
+      await axios(`${process.env.NEXT_PUBLIC_API_URL}/sent-letters`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      }).then((res) => res.data),
   });
 
   const mockUpData = [
     {
-      title: '편지1',
+      title: '내 사랑하는 남자친구에게',
       img: 'img1',
       id: 1,
-    },
-    {
-      title: '편지2',
-      img: 'img2',
-      id: 2,
-    },
-    {
-      title: '편지3',
-      img: 'img3',
-      id: 3,
-    },
-    {
-      title: '편지4',
-      img: 'img4',
-      id: 4,
     },
   ];
 
   const toNames = ['개발진', '권호정', '개발팀'];
 
+  if (isPending) return <Box>Loading...</Box>;
+
   return (
     <VStack gap={'1rem'} w={'80%'}>
       <Header />
-      <HStack justify={'flex-start'} w={'100%'}>
+      <VStack justify={'center'} w={'100%'}>
         <Heading textColor={'#FF6000'}>보관함</Heading>
-      </HStack>
-      <HStack w={'100%'} alignItems={'center'} justifyContent={'space-between'}>
         <Text textColor={'#FF8B45'} fontWeight={'bold'}>
           보관 가능 편지 {mockUpData.length}/6
         </Text>
+      </VStack>
+      <HStack w={'100%'} alignItems={'center'} justifyContent={'flex-end'}>
         <HStack gap={1}>
           <Select
             disabled
@@ -86,7 +87,7 @@ const View = () => {
       </HStack>
       <Box w={'100%'} px={10} py={2}>
         <Grid templateColumns="repeat(4, 1fr)" gap={6}>
-          {mockUpData.map((data, index) => (
+          {data2?.map((data: Message, index: number) => (
             <GridItem
               w="100%"
               h="50vh"
@@ -96,11 +97,16 @@ const View = () => {
               <VStack h={'100%'}>
                 <Link href={`/letter/view/${data?.id}`}>
                   <Box flexGrow={1}>
-                    <Image src={`/bg${index + 1}.jpg`} />
+                    <Image src={`/bg${(index % 4) + 1}.jpg`} />
                   </Box>
                 </Link>
                 <HStack align={'center'}>
-                  <Box h={'2rem'}>{data?.title}</Box>
+                  <Box h={'2rem'} fontWeight={'bold'}>
+                    제목: {data?.title}
+                  </Box>
+                </HStack>
+                <HStack align={'center'}>
+                  <Box h={'2rem'}>To. {data?.recipient}</Box>
                 </HStack>
               </VStack>
             </GridItem>
